cf11bc13fb1a7c8638912a528e43f1a7f0bd3186
SerkanS26 <s.safran84@gmail.com>
2024-10-03 14:47:44 +0200

    Update start script in package.json to configure server host

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: "0.0.0.0",
-  },
+  // server: {
+  //   port: 3000,
+
+  // },
 
   plugins: [react()],
 });
